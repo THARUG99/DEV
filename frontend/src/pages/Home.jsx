f565f4036fe790d3803f3115dadf62386cbfa8aa
THARUG99 <73222300+THARUG99@users.noreply.github.com>
2024-03-19 01:11:22 +0530

    home build

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import heroimg1 from '../assets/images/heroimg1.png'
 import vec1 from '../assets/images/vec1.png'
+import vec2 from '../assets/images/vec2.png'
+import vec3 from '../assets/images/vec3.png'
+
+import { Link } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
 
 const Home = () => {
   return <> 
@@ -73,7 +78,7 @@ const Home = () => {
   </section>
   {/*-------------- Hero Section End--------------------*/}
 
-  <section>
+  <section className='body__sec'>
     <div className="container">
       <div className='lg:w-[470px] mx-auto'>
         <h2 className='heading text-center'>
@@ -91,6 +96,37 @@ const Home = () => {
         </div>
         <div className='mt-[30px]'>
           <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find a Doctor</h2>
+          <p>World-class care for everyone.Our Health System offers unmatched,Expert health care from the lab to hospital</p>
+
+          <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover '>
+            <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+          </Link>
+        </div>
+      </div>
+
+      <div className="py-[30px] px-5 ">
+        <div className='flex items-center justify-center'>
+          <img src={vec3} alt="" />
+        </div>
+        <div className='mt-[30px]'>
+          <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Patients</h2>
+          <p>World-class care for everyone.Our Health System offers unmatched,Expert health care from the lab to hospital</p>
+
+          <Link to='/patients' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover '>
+            <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+          </Link>
+        </div>
+      </div>
+
+      <div className="py-[30px] px-5 ">
+        <div className='flex items-center justify-center'>
+          <img src={vec2} alt="" />
+        </div>
+        <div className='mt-[30px]'>
+          <h2 className='text-[26px] leading-9 text-headingColor font-[700] text-center'>Find Us</h2>
+          <p>World-class care for everyone.Our Health System offers unmatched,Expert health care from the lab to hospital</p>
+
+
         </div>
       </div>
 
