8657a331278ab9cfcc7ba1c0ed5f1c76cb51ae46
THARUG99 <73222300+THARUG99@users.noreply.github.com>
2024-03-21 22:52:21 +0530

    Doctor Details Update

--- frontend/src/assets/data/doctors.js
+++ frontend/src/assets/data/doctors.js
@@ -5,7 +5,7 @@ import doctorImg03 from "../images/doctor-img03.png";
 export const doctors = [
   {
     id: "01",
-    name: "Dr. Alfaz Ahmed",
+    name: "Dr. Charith Asalanka",
     specialization: "Surgeon",
     avgRating: 4.8,
     totalRating: 272,
@@ -15,7 +15,7 @@ export const doctors = [
   },
   {
     id: "02",
-    name: "Dr. Saleh Mahmud",
+    name: "Dr. Kusal Mendis",
     specialization: "Neurologist",
     avgRating: 4.8,
     totalRating: 272,
@@ -25,7 +25,7 @@ export const doctors = [
   },
   {
     id: "03",
-    name: "Dr. Farid Uddin",
+    name: "Dr. Pathum Nissanka",
     specialization: "Dermatologist",
     avgRating: 4.8,
     totalRating: 272,
