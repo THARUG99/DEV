6b7f2f466cb44ae41e2ab2913add5b16473ccb64
THARUG99 <73222300+THARUG99@users.noreply.github.com>
2024-04-18 04:13:43 +0530

    Review System & Authentications

---
+++ backend/Controllers/reviewController.js
@@ -0,0 +1,37 @@
+import Review from '../models/ReviewSchema.js'
+import Doctor from '../models/DoctorSchema.js'
+
+
+// All Reviews
+
+export const getAllReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    res.status(200).json({success:true, message: 'Reviews', data: reviews});
+  } catch (error) {
+    res.status(500).json({success:false, message: 'No reviews found'});
+  }
+}
+
+// Create a new review
+
+export const createReview = async (req, res) => {
+
+    if(!req.body.doctor) req.body.doctor = req.params.doctorId
+    if(!req.body.user) req.body.user = req.userId
+
+    const newReview = new Review(req.body)
+
+  try {
+    const savedReview = await newReview.save()
+
+    await Doctor.findByIdAndUpdate(req.body.doctor, {
+        $push: {
+            reviews: savedReview._id
+        }
+    })
+    res.status(200).json({success:true, message: 'Review successfully submitted ', data:savedReview});
+  } catch (error) {
+    res.status(500).json({success:false, message: 'Error submitting review'})
+  }
+}
